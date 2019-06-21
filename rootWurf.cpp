c35170daf61c0a60cac44a9a3a5140ed828e9a7a
lukasKaufmann <50980969+lukasKaufmann@users.noreply.github.com>
2019-06-21 20:40:59 +0200

    Steigung des Wurfes mit Reibung
    Aufwärtsbewegung des Wurfes mit Reibung wird anhand von einzelnen Wertepaaren  geplottet.

--- rootWurf.cpp
+++ rootWurf.cpp
@@ -22,14 +22,32 @@ class bahnkurve {
 	public:
 	///Funktion zum Plotten der Wurfparabel
 	void plot_bahn (double v, double w) {
-		float x;
+		float x; float vy = v * sin(w); float vx = v * cos(w);
 		statischeDaten h;
-		h.reichweite(v, w);
-		///Erstellt Wurfparabel mit Parametern 0 = Abwurfgeschw. v und 1 = Abwurfwinkel w
-		TF1 *f1 = new TF1("f1", "x*tan([1]) - 9.81/(2*[0]*[0]*cos([1])*cos([1])) *x*x", 0., h);
+		double d = h.reichweite(v, w);
+		///Erstellt Wurfparabel (ohne Reibung) mit Parametern 0 = Abwurfgeschw. v und 1 = Abwurfwinkel w
+		TF1 *f1 = new TF1("f1", "x*tan([1]) - 9.81/(2*[0]*[0]*cos([1])*cos([1])) *x*x", 0., d);
 		f1->SetParameter(0, v);
 		f1->SetParameter(1, w);
-		f1->Draw();
+		//f1->Draw();
+		///Wurf in Luft
+		float k = 6 * Pi * 17.1 * 0.000001 * v * 1/15; ///Reibungsfaktor
+		double g = 9.81;
+		float tU = -1/k * log( g/(k * (vy + g/k) ); ///Umkehrzeitpunkt
+		
+		ofstream outputFile;
+		outputFile.open("reibung_bahn.txt");
+	        for (float t = 0; t/10 < tU; t++) {
+                	float x_t = -vx/k * exp(-k * t/10) + vx/k;
+                        float y_t = -(vy * k + g)/(k*k) * exp(-k * t/10) - (g/k * t/10) + (vy * k + g)/(k*k);
+                        //cout << x_t << "  " << y_t << endl;
+                        outputFile << x_t << "  " << y_t << endl;
+                };
+                outputFile.close();
+                TGraph *gr = new TGraph("reibung.txt");
+                f1->Draw();
+                gr->Draw("same");
+
 	}
 };
 
