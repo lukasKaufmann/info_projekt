48b6a9692c863ded61b4b3b900bdb1fdab74024c
lukasKaufmann <50980969+lukasKaufmann@users.noreply.github.com>
2019-06-23 13:47:47 +0200

    statischeDaten header file geschrieben
    Header-File für die Klasse statischeDaten, die die Reichweite des reibungsfreien Wurfes (benötigt für Plot) berechnet.

---
+++ statischeDaten.hpp
@@ -0,0 +1,16 @@
+#ifndef STATISCHEDATEN_HPP__
+#define STATISCHEDATEN_HPP__
+
+#include<iostream>
+
+class statischeDaten{
+        public:
+        statischeDaten(); ///Konstruktor der Klasse
+        ~statischeDaten(); ///Destruktor der Klasse
+        
+        ///Funktion zur Berechnung der Reichweite des
+        ///reibungsfreien Wurfes
+        double reichweite(double v, double w);
+};
+
+#endif //STATISCHEDATEN_HPP__
