6ea28894e0145f2b169474c1e4c81c16ed10bbf3
lukasKaufmann <50980969+lukasKaufmann@users.noreply.github.com>
2019-06-27 00:45:56 +0200

    Flugdauer Methode definiert.

--- statischeDaten.cpp
+++ statischeDaten.cpp
@@ -14,4 +14,9 @@ statischeDaten::~statischeDaten(){
 ///Wurfweite berechnen:
 double statischeDaten::reichweite(double v, double w){
         return ( (v*v)/9.81 * sin(2*w) );
+}      
+///Flugdauer berechnen:
+double statischeDaten::flugdauer(double v, double w){
+        return (2/9.81 * v * sin(w));
 }
+
