3a10a5df7eef9490b24bcf15870c2f307e66265a
christinaMeinert <50980983+christinaMeinert@users.noreply.github.com>
2019-06-28 09:26:49 +0200

    Radius und Masse hinzugefuegt. Ueberpruefung der Eingabe.

--- rootWurf.cpp
+++ rootWurf.cpp
@@ -14,10 +14,31 @@ void rootWurf () {
 	double v; double w; double wRad;
 
 	///Eingabe der erforderlichen Daten (Geschw. , Winkel)
-	cout << "Enter velocity [m/s]:  " << endl;
+	cout << "Gib eine Abwurfgeschwindigkeit [m/s] ein:  " << endl;
 	cin >> v;
-	cout << "Enter angle between 0 and 90 degrees:  " << endl;
+	if (v <= 0) {
+		cout << "Du wirfst deine Kugel nach hinten. Hast du vorher geschaut, ob da jemand steht? Bitte korrigiere deine Eingabe der Geschwindigkeit!" << endl;
+		cin >> v;
+	}
+	cout << "Gib einen Abwurfwinkel zwischen 0 und 90 Grad ein:  " << endl;
 	cin >> wRad; w = (wRad * PI/180);
+	if (w <= 0 || w >= PI/2){
+		cout << "Eingabe des Winkels kann nicht verwertet werden. Bitte gib einen Winkel zwischen 0 und 90 Grad ein!" << endl;
+		cin >> wRad; w = (wRad * PI/180);
+	}
+	cout >> "Gib eine Masse [kg] ein:  " << endl;
+	cin >> m;
+	if (m <= 0) {
+		cout << "Was ist negative Masse? Bitte korrigiere deine Eingabe der Masse!" << endl;
+		cin >> m;
+	}
+	cout >> "Gib einen Radius [m] ein:  " << endl;
+	cin >> r
+	if (r<=0) {
+		cout << "Du hast einen negativen Radius eingegeben. Bitte gib einen sinnvollen Radius ein!" << endl;
+		cin >> r;
+	}
+
 
 	statischeDaten d ;
 	cout << "Reichweite des Wurfes (ohne Reibung):  ";
