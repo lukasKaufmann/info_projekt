c34971ba062bac4b677373d4fbac38422ae70dc4
lukasKaufmann <50980969+lukasKaufmann@users.noreply.github.com>
2019-07-01 19:48:05 +0200

    Kommentare entfernt

--- statischeDaten.cpp
+++ statischeDaten.cpp
@@ -3,18 +3,17 @@
 
 ///Konstruktor definieren:
 statischeDaten::statischeDaten(){
-        //wüsste nicht, was der wichtiges machen müsste
 }
 
 ///Destruktor definieren:
 statischeDaten::~statischeDaten(){
-        //hier auch nicht
 }
 
 ///Wurfweite berechnen:
 double statischeDaten::reichweite(double v, double w){
         return ( (v*v)/9.81 * sin(2*w) );
-}      
+} 
+
 ///Flugdauer berechnen:
 double statischeDaten::flugdauer(double v, double w){
         return (2/9.81 * v * sin(w));
