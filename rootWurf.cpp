6cd6d440ce0b97fd31471c790459d1cc81ffd8b7
lukasKaufmann <50980969+lukasKaufmann@users.noreply.github.com>
2019-07-01 19:30:56 +0200

    zum compilen mit g++ angepasst

--- rootWurf.cpp
+++ rootWurf.cpp
@@ -7,9 +7,7 @@
 
 using namespace std;
 
-//#define PI 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
-
-void rootWurf () {
+int main rootWurf () {
 
 	double v, w, wRad, m, r;
 
@@ -54,4 +52,6 @@ void rootWurf () {
 	///Plotten der Bahnkurve mit den eingegebenen Parametern
 	bahnkurve s;
 	s.plot(v, wRad, m, r);
+	
+	return 0;
 }
