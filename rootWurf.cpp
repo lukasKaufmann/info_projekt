26d71e9c5f4987e7cb785ac7ad2931ac33e9382c
lukasKaufmann <50980969+lukasKaufmann@users.noreply.github.com>
2019-07-01 19:16:19 +0200

    schleifen und bezeichnungen angepasst

--- rootWurf.cpp
+++ rootWurf.cpp
@@ -16,25 +16,27 @@ void rootWurf () {
 	///Eingabe der erforderlichen Daten (Geschw. , Winkel)
 	cout << "Gib eine Abwurfgeschwindigkeit [m/s] ein:  " << endl;
 	cin >> v;
-	if (v <= 0) {
+	while (v <= 0) {
 		cout << "Du wirfst deine Kugel nach hinten. Hast du vorher geschaut, ob da jemand steht? Bitte korrigiere deine Eingabe der Geschwindigkeit!" << endl;
 		cin >> v;
 	}
+	
 	cout << "Gib einen Abwurfwinkel zwischen 0 und 90 Grad ein:  " << endl;
-	cin >> wRad; w = (wRad * PI/180);
-	if (w <= 0 || w >= PI/2){
+	if (w <= 0 || w >= 90){
 		cout << "Eingabe des Winkels kann nicht verwertet werden. Bitte gib einen Winkel zwischen 0 und 90 Grad ein!" << endl;
-		cin >> wRad; w = (wRad * PI/180);
+		cin >> w;
 	}
+	wRad = (w * PI/180);
+	
 	cout >> "Gib eine Masse [kg] ein:  " << endl;
 	cin >> m;
-	if (m <= 0) {
+	while (m <= 0) {
 		cout << "Was ist negative Masse? Bitte korrigiere deine Eingabe der Masse!" << endl;
 		cin >> m;
 	}
 	cout >> "Gib einen Radius [m] ein:  " << endl;
 	cin >> r
-	if (r<=0) {
+	while (r<=0) {
 		cout << "Du hast einen negativen Radius eingegeben. Bitte gib einen sinnvollen Radius ein!" << endl;
 		cin >> r;
 	}
@@ -42,11 +44,11 @@ void rootWurf () {
 
 	statischeDaten d ;
 	cout << "Reichweite des Wurfes (ohne Reibung):  ";
-	cout << d.reichweite(v, w) << endl;
+	cout << d.reichweite(v, wRad) << endl;
 
 	statischeDaten t;
 	cout << "Dauer des Wurfes(ohne Reibung):  ";
-	cout << t.flugdauer(v, w) << endl;
+	cout << t.flugdauer(v, wRad) << endl;
 
 	bahnkurve s;
 	s.plot(v, w);
