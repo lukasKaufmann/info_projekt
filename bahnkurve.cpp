fe5e0e3be2b6fabb17006bf7930acd2b11a9f634
lukasKaufmann <50980969+lukasKaufmann@users.noreply.github.com>
2019-07-03 17:53:48 +0200

    jetzt malt er die Bahnkurven, Legende hinzugefügt

--- bahnkurve.cpp
+++ bahnkurve.cpp
@@ -2,8 +2,11 @@
 #include "math.h"
 #include "TF1.h"
 #include "TLegend.h"
+#include "TApplication.h"
+#include "TCanvas.h"
 
 using namespace std;
+
 #define PI 3.141592653
 
 bahnkurve::bahnkurve() {
@@ -13,10 +16,15 @@ bahnkurve::~bahnkurve() {
 }
 ///Diese Funktion erstellt die Flugbahnen fuer beide Wuerfe und plottet sie in ein Fenster
 void bahnkurve::plot(double v, double w, double m, double r) {
+        ///Grundlegende Variablen
         double x; double vy = (v * sin(w)); double vx = (v * cos(w));
         double g = 9.81;
         statischeDaten h;
         double d = h.reichweite(v, w);
+        
+        ///Erstellen eines TCanvas, in dem die Bahnkurven angezeigt werden
+        TApplication *a1 = new TApplication("a1", 0, 0);
+        TCanvas *c1 = new TCanvas("c1", "Bahnkurven", 750, 500);
         
         ///Flugbahn ohne Reibung
         TF1 *f1 = new TF1("f1", "x * tan([1]) - [2]/(2*[0]*[0]*cos([1])*cos([1])) *x*x", 0., d);
@@ -29,13 +37,20 @@ void bahnkurve::plot(double v, double w, double m, double r) {
         f2->SetParameter(0, vx); f2->SetParameter(1, vy); f2->SetParameter(2, g); f2->SetParameter(3, k);
         f2->SetLineColor(4);
         f1->SetTitle("Bahnkurven");
-        f1->Draw();
+        f1->Draw("c1");
         f2->Draw("same");
         
         ///Erstellen einer Legende
-        //auto legend = new TLegend(0.1, 0.7, 0.48, 0.9);
-        //legend->SetHeader("LEGENDE", "C");
-        //legend->AddEntry(f1, "Bahnkurve ohne Reibung", "f");
-        //legend->AddEntry(f2, "Bahnkurve mit Reibung", "lep");
-        //legend->Draw("same");
+        auto legend = new TLegend(0.1, 0.7, 0.48, 0.9);
+        legend->SetHeader("LEGENDE", "C");
+        legend->AddEntry(f1, "Bahnkurve ohne Reibung", "L");
+        legend->AddEntry(f2, "Bahnkurve mit Reibung", "L");
+        legend->Draw("same");
+        
+        ///Erzeugen des TCanvas und speichern der Bahnkurven
+        c1->Update();
+        c1->Draw();
+        c1->SaveAs("plot.png");
+        
+        a1->Run();
 }
