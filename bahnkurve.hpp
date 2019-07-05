3a7155613abc445d37e2d81fcbe9bb2e002687a1
lukasKaufmann <50980969+lukasKaufmann@users.noreply.github.com>
2019-07-05 13:36:44 +0200

    Update bahnkurve.hpp

--- bahnkurve.hpp
+++ bahnkurve.hpp
@@ -6,9 +6,14 @@
 
 class bahnkurve: public statischeDaten {
         public:
-        bahnkurve();
-        ~bahnkurve();
+        bahnkurve(); ///Konstruktor der Klasse
+        ~bahnkurve(); ///Destruktor der Klasse
         
+        /**
+        *       Methode, die mit den eingelesenen Daten beide Plots
+        *       erstellt, in ein Canvas malt und anzeigt/ speichert
+        *       (genaue Vorgehensweise in bahnkurve.cpp).
+        */
         void plot(double v, double w, double m, double r);
 };
 
