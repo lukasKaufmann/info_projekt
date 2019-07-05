9cdad934ae4871b179751a8d21c7130aa90537c2
lukasKaufmann <50980969+lukasKaufmann@users.noreply.github.com>
2019-07-05 13:43:49 +0200

    Kommentare angepasst

--- bahnkurve.cpp
+++ bahnkurve.cpp
@@ -9,18 +9,21 @@ using namespace std;
 
 #define PI 3.141592653
 
+///Konstruktor definieren:
 bahnkurve::bahnkurve() {
 }
 
+///Destruktor definieren:
 bahnkurve::~bahnkurve() {
 }
-///Diese Funktion erstellt die Flugbahnen fuer beide Wuerfe und plottet sie in ein Fenster
+
+///Plots erstellen/ speichern
 void bahnkurve::plot(double v, double w, double m, double r) {
         ///Grundlegende Variablen
         double x; double vy = (v * sin(w)); double vx = (v * cos(w));
         double g = 9.81;
         statischeDaten h;
-        double d = h.reichweite(v, w);
+        double d = h.reichweite(v, w); ///reichweite Methode aus statischeDaten
         
         ///Erstellen eines TCanvas, in dem die Bahnkurven angezeigt werden
         TApplication *a1 = new TApplication("a1", 0, 0);
@@ -31,13 +34,13 @@ void bahnkurve::plot(double v, double w, double m, double r) {
         f1->SetParameter(0, v); f1->SetParameter(1, w); f1->SetParameter(2, g);
         
         ///Flugbahn mit Reibung
-        double k = (6 * PI * 17.1 * 0.000001 * r/m);
+        double k = (6 * PI * 17.1 * 0.000001 * r/m); ///Reibungsfaktor/Masse nach STOKES
         
         TF1 *f2 = new TF1("f2", "[2]/([3]*[3]) * log(1 - [3]/[0] *x) + ([1] + [2]/[3]) * x/[0]", 0., d);
         f2->SetParameter(0, vx); f2->SetParameter(1, vy); f2->SetParameter(2, g); f2->SetParameter(3, k);
         f2->SetLineColor(4);
         f1->SetTitle("Bahnkurven");
-        f1->Draw("c1");
+        f1->Draw("c1"); 
         f2->Draw("same");
         
         ///Erstellen einer Legende
